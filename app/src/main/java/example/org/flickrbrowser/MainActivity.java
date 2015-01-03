8e92f680a970bc201dc3cbcf0ab05f2964ad5fe3
Scott Busche <swbusche@gmail.com>
2015-01-03 17:01:50 -0600

    Remove fragment code

--- app/src/main/java/example/org/flickrbrowser/MainActivity.java
+++ app/src/main/java/example/org/flickrbrowser/MainActivity.java
@@ -15,11 +15,6 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }
         GetFlickrJsonData getFlickrJsonData =
                 new GetFlickrJsonData("Lollipop,android", true);
         getFlickrJsonData.execute();
@@ -47,19 +42,4 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
-        }
-    }
 }
